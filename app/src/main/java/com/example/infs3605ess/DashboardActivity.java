a122b489c89f7e252e5c285c8384f0b741960b2b
Allison <z5183911@ad.unsw.edu.au>
2022-07-09 18:09:18 +0800

    Choose Image + OCR

--- app/src/main/java/com/example/infs3605ess/DashboardActivity.java
+++ app/src/main/java/com/example/infs3605ess/DashboardActivity.java
@@ -2,13 +2,28 @@ package com.example.infs3605ess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private Button ocr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        ocr=findViewById(R.id.btt_OCR);
+        ocr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ScanActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
